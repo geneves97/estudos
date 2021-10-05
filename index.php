8f21cb0eb633c539f09ec5c2745bc19eb678f16b
George Neves <50254856+geneves97@users.noreply.github.com>
2021-10-05 18:20:16 -0300

    novas formas de gerar o numero
    foram adicionadas mais 3 formas de gerar o token

--- index.php
+++ index.php
@@ -1,17 +1,33 @@
 <?php
     #função geradora de string
     
-    $codigoDeVerificacao = array();
-    $i = 0;
-    for($i; $i<3; $i++){
-        $num = random_int(1,100);
-        $codigoDeVerificacao[] = $num;    
-    };
     
+
+
+
+    // $codigoDeVerificacao = array();
+    // $i = 0;
+    // for($i; $i<3; $i++){
+    //     $num = random_int(1,100);
+    //     $codigoDeVerificacao[] = $num;    
+    // };
+
+
+    $str = bin2hex(random_bytes(4) );
+    $arr = str_split($str);
+    echo $str;
+    echo "\n";
+    var_dump($arr); 
     
-    print_r ($codigoDeVerificacao);
+    // print_r ($codigoDeVerificacao);
 
     // echo "\n\t";
     // echo bin2hex($bytes);
+
+    
+
+    // var_dump(bin2hex(openssl_random_pseudo_bytes(4)));
+
+    // var_dump(md5(uniqid(mt_rand(1,10),true)) );
        
 ?>
