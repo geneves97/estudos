173375f9bec0655135396c26d7e558e454474f88
George Neves <georgeneves97@gmail.com>
2021-10-05 16:57:53 -0300

    código de verificação
    criei um codigo de verificação em php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-primeira alteração git bash  111111
+<?php
+    #função geradora de string
+    
+    $codigoDeVerificacao = array();
+    $i = 0;
+    for($i; $i<3; $i++){
+        $num = random_int(1,100);
+        $codigoDeVerificacao[] = $num;    
+    };
+    
+    
+    print_r ($codigoDeVerificacao);
+
+    // echo "\n\t";
+    // echo bin2hex($bytes);
+       
+?>
